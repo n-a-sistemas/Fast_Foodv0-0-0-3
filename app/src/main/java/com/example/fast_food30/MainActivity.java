d665c8339c150b32cbb92f1e03749557ba6727c9
Natalia_Valerio <nat.valerio95@gmail.com>
2019-09-24 15:28:22 -0300

    Alterando o menu principal.

--- app/src/main/java/com/example/fast_food30/MainActivity.java
+++ app/src/main/java/com/example/fast_food30/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.fast_food30;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.example.fast_food30.modelo.Usuario;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
