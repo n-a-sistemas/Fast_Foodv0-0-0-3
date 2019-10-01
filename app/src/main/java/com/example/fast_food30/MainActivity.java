a78ffcb840e851f3661467ea65c189912d102dff
LhT7 <leozin.h.t@gmail.com>
2019-10-01 16:39:07 -0300

    Cupom ListView

--- app/src/main/java/com/example/fast_food30/MainActivity.java
+++ app/src/main/java/com/example/fast_food30/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         textViewVida = findViewById(R.id.text_view_vida);
         imageViewCupom = findViewById(R.id.image_view_cupom);
         textViewPontos = findViewById(R.id.text_view_pontos);
-        listView = findViewById(R.id.list_view);
+        listView = findViewById(R.id.list_view_cupom);
     }
 
 
